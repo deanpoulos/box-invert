be35dd4f06e742942e3e61f19d3e36449cc7256c
Dean Poulos <dean.poulos7@gmail.com>
2021-03-27 16:52:21 +1100

    Change middle-mouse-click to CTRL-left-mouse-click.

--- src/box_invert/mouse_callback.py
+++ src/box_invert/mouse_callback.py
@@ -21,6 +21,21 @@ def draw_circle(event,x,y,flags,param):
         else:
             cv2.circle(img,(x,y),5,(255,255,255),-1)
 
+    elif event == cv2.EVENT_LBUTTONDOWN and flags == cv2.EVENT_FLAG_CTRLKEY+1:
+        ix,iy = x,y
+
+    elif event == cv2.EVENT_LBUTTONUP and flags == cv2.EVENT_FLAG_CTRLKEY+1:
+        from box_invert import smart_invert
+        curr_img = smart_invert.img.copy().astype(np.uint8)
+        mask = np.zeros(curr_img.shape).astype(np.uint8)
+        masked_img = np.zeros(curr_img.shape).astype(np.uint8)
+        cv2.rectangle(mask,(ix,iy),(x,y),(255,255,255),-1)
+        for i in range(curr_img.shape[0]):
+            for j in range(curr_img.shape[1]):
+                if mask[i][j][2] == 255: masked_img[i][j] = curr_img[i][j]
+                
+        contours = get_contours(masked_img)
+        cv2.fillPoly(img, pts =contours, color=(0,0,0))
 
     elif event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
@@ -33,18 +48,4 @@ def draw_circle(event,x,y,flags,param):
         else:
             cv2.circle(img,(x,y),5,(0,0,0),-1)
 
-    elif event == cv2.EVENT_MBUTTONDOWN:
-        ix,iy = x,y
 
-    elif event == cv2.EVENT_MBUTTONUP:
-        from box_invert import smart_invert
-        curr_img = smart_invert.img.copy().astype(np.uint8)
-        mask = np.zeros(curr_img.shape).astype(np.uint8)
-        masked_img = np.zeros(curr_img.shape).astype(np.uint8)
-        cv2.rectangle(mask,(ix,iy),(x,y),(255,255,255),-1)
-        for i in range(curr_img.shape[0]):
-            for j in range(curr_img.shape[1]):
-                if mask[i][j][2] == 255: masked_img[i][j] = curr_img[i][j]
-                
-        contours = get_contours(masked_img)
-        cv2.fillPoly(img, pts =contours, color=(0,0,0))
